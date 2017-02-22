d45299b851290bba4c2d5431c91b1e3954d1f3e0
Game110AM <Game110AM@605-110-STU06>
2017-02-22 08:11:32 -0700

    comments

--- Project5/Project5/Source.cpp
+++ Project5/Project5/Source.cpp
@@ -6,26 +6,26 @@ int main() {
 	int m;
 	double sum;
 	cout << "which form of math do you want to do?" << endl;
-	cout << "You can choose between addition(a), subtraction(s), multiplication(m), or division(d)." << endl;
-		cin >> a;
+	cout << "You can choose between addition(a), subtraction(s), multiplication(m), or division(d)." << endl;//gives user choices
+		cin >> a;//takes choice
 		cout << "What two numbers would you like to use?" << endl;
 		cin >> s;
-		cin >> m;
-		switch (a) {
+		cin >> m;//takes numbers
+		switch (a) {//different operations
 		case 'a':
-			sum = m + s;
+			sum = m + s;//adds
 			cout << s << " + " << m << " = " << sum << endl;
 			break;
 		case's':
-			sum = s - m;
+			sum = s - m;//subtracts
 			cout << s << " - " << m << " = " << sum << endl;
 			break;
 		case'm':
-			sum = s*m;
+			sum = s*m;//multiplies
 			cout << s << "*" << m << "=" << sum << endl;
 			break;
 		case 'd':
-			sum = s / m;
+			sum = s / m;//divides
 			cout << s << " / " << m << " = " << sum << endl;
 			break;
 		default:
